b736fb7d6a006bd776698d58f43ebd6a8e1738f9
Julian Kuerby <kuerby@adesso-mobile.de>
2019-03-11 12:04:43 +0100

    Add PostsApi used as the Retrofit interface.

---
+++ app/src/main/java/de/adessomobile/postbookchallenge/client/api/PostsApi.kt
@@ -0,0 +1,25 @@
+package de.adessomobile.postbookchallenge.client.api
+
+import de.adessomobile.postbookchallenge.client.models.CommentDto
+import de.adessomobile.postbookchallenge.client.models.PostDto
+import kotlinx.coroutines.Deferred
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+/**
+ * RetrofitInterface for the posts API of https://jsonplaceholder.typicode.com
+ */
+interface PostsApi {
+
+    /**
+     * Get a list of all posts of the user with the given [userId].
+     */
+    @GET("posts")
+    fun listPosts(@Query("userId") userId: Int): Deferred<List<PostDto>>
+
+    /**
+     * Get a list of all comments for a post with the given [postId].
+     */
+    @GET("comments")
+    fun listComments(@Query("postId") postId: Int): Deferred<List<CommentDto>>
+}
