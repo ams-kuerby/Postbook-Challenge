776cc6d80c0931e1e3deafb03e63ddee408f2932
Julian Kuerby <kuerby@adesso-mobile.de>
2019-03-12 11:13:35 +0100

    Add DataBinding for LoginActivity and LoginViewModel

--- app/src/main/java/de/adessomobile/postbookchallenge/ui/login/LoginActivity.kt
+++ app/src/main/java/de/adessomobile/postbookchallenge/ui/login/LoginActivity.kt
@@ -2,7 +2,9 @@ package de.adessomobile.postbookchallenge.ui.login
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
 import de.adessomobile.postbookchallenge.R
+import de.adessomobile.postbookchallenge.databinding.ActivityLoginBinding
 import org.koin.android.ext.android.inject
 
 /**
@@ -14,6 +16,10 @@ class LoginActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
+        DataBindingUtil.setContentView<ActivityLoginBinding>(this, R.layout.activity_login)
+            .let {
+                it.viewModel = viewModel
+                it.lifecycleOwner = this
+            }
     }
 }
