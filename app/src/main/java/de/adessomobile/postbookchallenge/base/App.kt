ed80f928b860dbfa4eb1cb36a2e6dac34318e588
Julian Kuerby <kuerby@adesso-mobile.de>
2019-03-12 11:37:10 +0100

    Add Timber

--- app/src/main/java/de/adessomobile/postbookchallenge/base/App.kt
+++ app/src/main/java/de/adessomobile/postbookchallenge/base/App.kt
@@ -1,18 +1,27 @@
 package de.adessomobile.postbookchallenge.base
 
 import android.app.Application
+import de.adessomobile.postbookchallenge.BuildConfig
 import de.adessomobile.postbookchallenge.base.coroutines.coroutineKoinModule
 import de.adessomobile.postbookchallenge.data.dataKoinModule
 import de.adessomobile.postbookchallenge.repository.repositoryKoinModule
 import de.adessomobile.postbookchallenge.ui.login.loginKoinModule
 import org.koin.android.ext.android.startKoin
+import timber.log.Timber
 
 class App : Application() {
 
     override fun onCreate() {
         super.onCreate()
 
+        initTimber()
         initKoin()
+    }
+
+    private fun initTimber() {
+        if (BuildConfig.DEBUG) {
+            Timber.plant(Timber.DebugTree())
+        }
     }
 
     private fun initKoin() {
