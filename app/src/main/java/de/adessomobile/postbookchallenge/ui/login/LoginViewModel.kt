8b2734d50a8bf44a7ed7c00e547135bc425a859b
Julian Kuerby <kuerby@adesso-mobile.de>
2019-03-12 11:01:06 +0100

    Add LoginViewModel

---
+++ app/src/main/java/de/adessomobile/postbookchallenge/ui/login/LoginViewModel.kt
@@ -0,0 +1,13 @@
+package de.adessomobile.postbookchallenge.ui.login
+
+import de.adessomobile.postbookchallenge.base.BaseViewModel
+import de.adessomobile.postbookchallenge.base.coroutines.CoroutineContextProvider
+
+/**
+ * Architecture Component ViewModel for the LoginActivity
+ */
+class LoginViewModel(
+    coroutineContextProvider: CoroutineContextProvider
+) : BaseViewModel(coroutineContextProvider) {
+
+}
