0b967e0a5ab95372f7473e0cc1e52258b1b149eb
Julian Kuerby <kuerby@adesso-mobile.de>
2019-03-12 10:43:34 +0100

    Add LoginActivity and layout

---
+++ app/src/main/java/de/adessomobile/postbookchallenge/ui/login/LoginActivity.kt
@@ -0,0 +1,17 @@
+package de.adessomobile.postbookchallenge.ui.login
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import de.adessomobile.postbookchallenge.R
+
+/**
+ * Activity to login with a userId.
+ */
+class LoginActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+
+}
